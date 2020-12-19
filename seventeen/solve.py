2189c9e929b973060f69748ba872ef9962842039
Kevin Kays <kkays@raybeam.com>
2020-12-19 17:45:17 -0500

    slightly faster

--- seventeen/solve.py
+++ seventeen/solve.py
@@ -1,12 +1,13 @@
+import copy
 import itertools
 
 
-def evaluate_cell(cube, cell_status, *dims):
+def evaluate_cell(cube, cell_status, active_cells, *dims):
   value = 0
   maxs = []
   mins = []
   layer = cube
-  while not isinstance(layer, str):
+  while not isinstance(layer, bool):
     try:
       mins.insert(0, min(layer.keys()))
       maxs.insert(0, max(layer.keys()))
@@ -23,17 +24,15 @@ def evaluate_cell(cube, cell_status, *dims):
     if any(m is not None and c + d > m for c, m, d in zip(dims, maxs, diffs)):
       continue
     try:
-      neighbor_status = cube
-      for dim, diff in zip(reversed(dims), reversed(diffs)):
-        neighbor_status = neighbor_status[dim + diff]
+      neighbor_status = tuple(dim + diff for dim, diff in zip(dims, diffs)) in active_cells
     except IndexError:
-      neighbor_status = '.'
-    if neighbor_status == '#':
+      neighbor_status = False
+    if neighbor_status:
       value += 1
-  if cell_status == '#':
-    return '#' if value in [2, 3] else '.'
-  if cell_status == '.':
-    return '#' if value == 3 else '.'
+  if cell_status:
+    return value in [2, 3]
+  if not cell_status:
+    return value == 3
   raise ValueError(f'Not a valid cell: {cell_status}')
 
 
@@ -45,8 +44,8 @@ def hash_grid(grid):
   return '\n'.join(''.join(row) for row in grid)
 
 
-def evaluate_cube(cube, active_cells):
-  new_cube = make_empty_cube(len(cube[0]))
+def evaluate_cube(cube, active_cells, depth):
+  new_cube = make_empty_hcube(len(cube[0]))
   new_active_cells = []
   to_check = set(active_cells)
   for dims in active_cells:
@@ -59,63 +58,57 @@ def evaluate_cube(cube, active_cells):
       new_row = new_row[dim]
       row = row[dim]
     cell_status = row[cell[0]]
-    value = evaluate_cell(cube, cell_status, *cell)
+    value = evaluate_cell(cube, cell_status, active_cells, *cell)
     new_row[cell[0]] = value
-    if value == '#':
+    if value:
       new_active_cells.append(cell)
   return new_cube, new_active_cells
 
 
 def make_empty_cube(dimension):
-  return {z: make_empty_grid(dimension) for z in range(-dimension, dimension + 1)}
+  return {d: make_empty_grid(dimension) for d in range(-dimension, dimension + 1)}
+
+
+def make_empty_hcube(dimension):
+  return {d: make_empty_cube(dimension) for d in range(-dimension, dimension + 1)}
 
 
 def make_empty_grid(dimension):
-  return [['.' for col in range(dimension)] for row in range(dimension)]
-
-
-def count_active_in_cube(cube):
-  total = 0
-  for grid in cube.values():
-    for row in grid:
-      for col in row:
-        if col == '#':
-          total += 1
-  return total
+  return [[False for col in range(dimension)] for row in range(dimension)]
 
 
 def inflate_grid(grid, buffer):
   size = (2 * buffer) + len(grid)
-  result = [['.'] * size for row in range(buffer)]
+  result = [[False] * size for row in range(buffer)]
   for row in grid:
-    result.append((['.'] * buffer) + list(row) + (['.'] * buffer))
+    result.append(([False] * buffer) + list(row) + ([False] * buffer))
   for row in range(buffer):
-    result.append(['.'] * size)
+    result.append([False] * size)
   return result
 
 
-def solve(filename, iterations):
+def solve(filename, iterations, depth):
   with open(filename, 'r') as f:
-    problem_grid = [list(row.strip()) for row in f.readlines()]
+    problem_grid = [[c == '#' for c in row.strip()] for row in f.readlines()]
 
   problem_grid = inflate_grid(problem_grid, 6)
 
-  problem_cube = make_empty_cube(len(problem_grid))
-  problem_cube[0] = problem_grid
+  problem_cube = make_empty_hcube(len(problem_grid))
+  problem_cube[0][0] = problem_grid
   active_cells = []
   for y, row in enumerate(problem_grid):
     for x, cell in enumerate(row):
-      if cell == '#':
-        active_cells.append((x, y, 0))
+      if cell:
+        active_cells.append((x, y, *[0 for i in range(depth)]))
 
   cube = problem_cube
   for i in range(iterations):
-    cube, active_cells = evaluate_cube(cube, active_cells)
+    cube, active_cells = evaluate_cube(cube, active_cells, depth)
     print(f'==========iteration: {i + 1}==========')
     # print(hash_cube(cube))
     print(len(active_cells))
   return len(active_cells)
 
 
-print(solve('dummy.txt', 6))
-# print(solve('input.txt', 6))
+print(solve('dummy.txt', 6, 2))
+# print(solve('input.txt', 6, 2))
