be49718f803708b2c7222eacb5bb3f2b5d3cf6b2
Kevin Kays <kkays@raybeam.com>
2020-12-19 13:46:00 -0500

    better, faster, more generic part 1

--- seventeen/solve.py
+++ seventeen/solve.py
@@ -1,19 +1,31 @@
-import collections
 import itertools
 
 
-def evaluate_cell(cube, row, col, z):
+def evaluate_cell(cube, cell_status, *dims):
   value = 0
-  min_z = min(cube.keys())
-  max_z = max(cube.keys())
-  cell_status = cube[z][row][col]
-  for z_diff, row_diff, col_diff in itertools.product(range(-1, 2), range(-1, 2), range(-1, 2)):
-    if row_diff == 0 and col_diff == 0 and z_diff == 0:
+  maxs = []
+  mins = []
+  layer = cube
+  while not isinstance(layer, str):
+    try:
+      mins.insert(0, min(layer.keys()))
+      maxs.insert(0, max(layer.keys()))
+      layer = layer[0]
+    except AttributeError:
+      mins.insert(0, 0)
+      maxs.insert(0, None)
+      layer = layer[0]
+  for diffs in itertools.product(*[range(-1, 2) for _ in dims]):
+    if all(d == 0 for d in diffs):
       continue
-    if row + row_diff < 0 or col + col_diff < 0 or z + z_diff < min_z or z + z_diff > max_z:
+    if any(c + d < m for c, m, d in zip(dims, mins, diffs)):
+      continue
+    if any(m is not None and c + d > m for c, m, d in zip(dims, maxs, diffs)):
       continue
     try:
-      neighbor_status = cube[z + z_diff][row + row_diff][col + col_diff]
+      neighbor_status = cube
+      for dim, diff in zip(reversed(dims), reversed(diffs)):
+        neighbor_status = neighbor_status[dim + diff]
     except IndexError:
       neighbor_status = '.'
     if neighbor_status == '#':
@@ -33,17 +45,25 @@ def hash_grid(grid):
   return '\n'.join(''.join(row) for row in grid)
 
 
-def evaluate_cube(cube):
+def evaluate_cube(cube, active_cells):
   new_cube = make_empty_cube(len(cube[0]))
-  for z, grid in cube.items():
-    new_grid = []
-    for (rownum, row) in enumerate(grid):
-      new_grid.append([
-          evaluate_cell(cube, rownum, colnum, z)
-          for (colnum, col) in enumerate(row)
-      ])
-    new_cube[z] = new_grid
-  return new_cube
+  new_active_cells = []
+  to_check = set(active_cells)
+  for dims in active_cells:
+    for diffs in itertools.product(*[range(-1, 2) for _ in dims]):
+      to_check.add(tuple(dim + diff for dim, diff in zip(dims, diffs)))
+  for cell in to_check:
+    row = cube
+    new_row = new_cube
+    for dim in reversed(cell[1:]):
+      new_row = new_row[dim]
+      row = row[dim]
+    cell_status = row[cell[0]]
+    value = evaluate_cell(cube, cell_status, *cell)
+    new_row[cell[0]] = value
+    if value == '#':
+      new_active_cells.append(cell)
+  return new_cube, new_active_cells
 
 
 def make_empty_cube(dimension):
@@ -79,21 +99,23 @@ def solve(filename, iterations):
     problem_grid = [list(row.strip()) for row in f.readlines()]
 
   problem_grid = inflate_grid(problem_grid, 6)
-  print(problem_grid)
 
   problem_cube = make_empty_cube(len(problem_grid))
   problem_cube[0] = problem_grid
+  active_cells = []
+  for y, row in enumerate(problem_grid):
+    for x, cell in enumerate(row):
+      if cell == '#':
+        active_cells.append((x, y, 0))
 
   cube = problem_cube
-  print(hash_cube(cube))
   for i in range(iterations):
-    cube = evaluate_cube(cube)
+    cube, active_cells = evaluate_cube(cube, active_cells)
     print(f'==========iteration: {i + 1}==========')
-    print(hash_cube(cube))
-    print(count_active_in_cube(cube))
-    # input('Press enter to continue.')
-  return count_active_in_cube(cube)
+    # print(hash_cube(cube))
+    print(len(active_cells))
+  return len(active_cells)
 
 
-# print(solve('dummy.txt', 6))
-print(solve('input.txt', 6))
+print(solve('dummy.txt', 6))
+# print(solve('input.txt', 6))
